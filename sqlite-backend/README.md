73a78ff85e229dca56107a148773f3f2276f717d
Khalid Elassaad <khalidelassaad@gmail.com>
2023-03-17 21:21:08 -0700

    code: add user info to backend js

--- sqlite-backend/README.md
+++ sqlite-backend/README.md
@@ -9,3 +9,5 @@ Express routing: https://expressjs.com/en/starter/basic-routing.html
 SQLite: https://sqlite.org/quickstart.html
 
 Prisma Express SQLite Example: https://github.com/prisma/prisma-examples/tree/latest/javascript/rest-express
+
+https://www.prisma.io/express
--- sqlite-backend/index.js
+++ sqlite-backend/index.js
@@ -5,10 +5,14 @@ const port = 8000
 const { PrismaClient } = require('@prisma/client')
 
 const prisma = new PrismaClient()
-// use `prisma` in your application to read and write data in your DB
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/user', async (req, res) => {
+  const posts = await prisma.user.findMany()
+  res.json(posts)
 })
 
 app.listen(port, () => {
