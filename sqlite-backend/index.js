ea109370e643634804e255c1cc6a84504bbc79bf
Khalid Elassaad <khalidelassaad@gmail.com>
2023-03-17 20:46:45 -0700

    code: trying to init sqlite

--- sqlite-backend/index.js
+++ sqlite-backend/index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const port = 8000
 
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
+// use `prisma` in your application to read and write data in your DB
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
